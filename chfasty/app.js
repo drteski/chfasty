1c549b4942cb6580f0deb1bf8f747c64d2c8b8dc
Maciej Malinowski <105872023+mejsiejdev@users.noreply.github.com>
2022-06-27 10:48:10 +0200

    Dodanie załączania lóg firm
    Po najechaniu kursorem myszy na logo firmy jest one załączane na koniec `#BRANDS` by się wyświetlało ponad innymi.

--- chfasty/app.js
+++ chfasty/app.js
@@ -1,46 +1,10 @@
 // const brandBox = Array.from(document.querySelectorAll('.BRAND_SELECTOR'));
-const brandBox = document.querySelectorAll('.LOGO');
-
-    const brandFrame = document.querySelector('#BRANDS').lastElementChild;
-
-console.log(brandBox);
-
-
-
+const brandBox = document.querySelectorAll(".LOGO");
 
 for (box = 0; box < brandBox.length; box++) {
-  console.log(brandBox[box])
-  brandBox[box].addEventListener('mouseenter', e => {
-    e.currentTarget.after(brandFrame)
-    console.log(brandFrame.childNodes);
+  const brands = document.getElementById("BRANDS");
+  brandBox[box].addEventListener("mouseenter", (e) => {
+    // Dodawanie wybranego elementu na koniec #BRANDS, by był wyżej niż inne.
+    brands.append(e.target)
   });
 }
-
-  
-  
-
-
-
-
-
-// const storeMapBox = document.querySelector('#BRANDS');
-
-//     box.addEventListener('mouseenter', e => {
-//       let boxId = `#${e.currentTarget.classList[0]}`;
-//       const selectedBox = document.querySelector('use');
-//       selectedBox.setAttribute('class', `${boxId.slice(1)}`);
-//       selectedBox.setAttribute('xlink:href', `${boxId}`);
-//       e.currentTarget.classList.add('BRAND_ACTIVE');
-
-//     });
-//     box.addEventListener('mouseleave', e => {
-//       e.currentTarget.classList.remove('BRAND_ACTIVE');
-//     });
-//   });
-// }
-
-// boxSelect();
-
-// for (i = 0; i <= brandBox2.length; i++) {
-//   brandFrame.removeChild(brandBox2[i])
-// }
